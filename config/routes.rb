99123f00c4e5bb1053f3c141bcde088e42d8bbc5
leonardo <leonardodias@ucl.br>
2022-03-04 19:50:20 -0300

    Add new routes for tracks. Add root route if authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users do
+    resources :tracks
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root :to => 'tracks#index', as: :authenticated_root
+  end
+  
   root to: "users#index"
 end
