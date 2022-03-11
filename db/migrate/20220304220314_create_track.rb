12ea2021b6dc161c3f68af83f8a648443419c8d7
leonardo <leonardodias@ucl.br>
2022-03-11 17:35:30 -0300

    Change int to float type and add fk in tracks table

--- db/migrate/20220304220314_create_track.rb
+++ db/migrate/20220304220314_create_track.rb
@@ -1,9 +1,8 @@
 class CreateTrack < ActiveRecord::Migration[6.1]
   def change
     create_table :tracks do |t|
-      t.integer :user_id
-      t.integer :longitude, array:true, default: []
-      t.integer :latitude, array:true, default: []
+      t.float :longitude, array:true, default: []
+      t.float :latitude, array:true, default: []
       t.integer :time
       t.text :extra
 
---
+++ db/migrate/20220310222201_add_user_ref_to_tracks.rb
@@ -0,0 +1,5 @@
+class AddUserRefToTracks < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :tracks, :user, null: false, foreign_key: true
+  end
+end
