d9b063e404ca070564b42cbfde84118ac710b81d
leonardo <leonardodias@ucl.br>
2022-03-11 17:37:53 -0300

    Add track_params to permit parameters and array params. Fix create method

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,31 @@
 class TracksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = current_user
+  end
+
+  def new
+    @user = current_user
+    @track = Track.new
+  end
+
+  def create
+    user = current_user
+    track = user.tracks.new(track_params)
+
+    if track.save
+      flash[:notice] = 'The track was saved successfully.'
+      redirect_to root_url
+    else
+      flash[:error] = 'Track not created'
+      render 'new'
+    end
+  end
+
+  private
+  
+  def track_params
+    params.require(:track).permit(:extra, { :latitude => [] }, { :longitude => [] }, :time)
+  end
 end
